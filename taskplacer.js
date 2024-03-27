77d05bb24e4b3b51c7d519d9bebeb33aa63f4fce
Karolina <163521962+robotdog95@users.noreply.github.com>
2024-03-27 23:08:08 +0100

    Update taskplacer.js

--- taskplacer.js
+++ taskplacer.js
@@ -240,6 +240,9 @@ dropZones.forEach(dropZone => {
 
 function handleDragOver(event) {
 event.preventDefault();
+const dropZoneId = event.target.id;
+const dropZoneElement = document.getElementById(dropZoneId);
+dropZoneElement.style.border = `3px solid`;
 }
 
 function handleDrop(event) {
@@ -250,7 +253,7 @@ const taskId = event.dataTransfer.getData('text/plain');
 const droppedTask = document.getElementById(taskId);
 // Handle the dropped task, update position, etc.
 const dropZonePosition = dropZoneElement.getBoundingClientRect(); // Get position of the drop zone
-const topPosition = dropZonePosition.top + window.scrollY + dropZonePosition.height/2;
+const topPosition = dropZonePosition.top + window.scrollY;
 droppedTask.style.top = `${topPosition}px`;
 // maybe add a bool to decide if the dropZone is already full??
       console.log("task has been moved to ",dropZoneId);
