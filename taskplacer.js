64f08ccb43b11b36074f37750ccc4eda91d91b28
Karolina <163521962+robotdog95@users.noreply.github.com>
2024-03-28 11:17:59 +0100

    Update taskplacer.js

--- taskplacer.js
+++ taskplacer.js
@@ -212,14 +212,25 @@ if (hourElementNow && hourElementNow.id === hourIdToLookFor) {
     
     //initialize array for cookies:
     const cookieArray = {};
-    
-    
+  
     // set a cookie
 function setCookie(name, value, days) {
   const expires = new Date();
   expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
   document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
 };
+    // get a cookie
+    function getCookie(name) {
+  const cookieName = `${name}=`;
+  const cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i].trim();
+    if (cookie.indexOf(cookieName) === 0) {
+      return cookie.substring(cookieName.length, cookie.length);
+    }
+  }
+  return null;
+}
 // DROP ZONE HANDLING----------------------------------------------------------------------
 const tasks = document.querySelectorAll('.task');
 
